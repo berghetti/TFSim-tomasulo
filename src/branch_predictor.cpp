3cb0a806b3d378bb00e263b2ad8ad903a5115b4e
Mayco <mayco.s.berghetti@gmail.com>
2022-06-18 15:33:09 -0400

    change initial state of original prediction of tfsim to 0

--- src/branch_predictor.cpp
+++ src/branch_predictor.cpp
@@ -3,13 +3,11 @@
 branch_predictor::branch_predictor(unsigned int t): n_bits(t)
 {
     max = (1<<n_bits)-1;
-    state = 1; // initial state 0 is not taken??
+    state = 0; // initial state 0 is not taken??
 }
 
 bool branch_predictor::predict()
 {
-  // TODO: logic BPB probably should be implemented here
-  //       need pc original of insturctions
     return state&(1<<(n_bits-1));
 }
 void branch_predictor::update_state(bool taken)
